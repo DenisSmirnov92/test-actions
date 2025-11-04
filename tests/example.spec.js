7dd3449df6e599d4f718c82415e07d7be6629d87
Denis Smirnov <dsmirnov@tula.co>
2025-11-04 20:50:02 +0100

    remove unnecessary waitForURL call in basic test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,7 +3,6 @@ import { test, expect } from '@playwright/test';
 test('basic test', async ({ page }) => {
   await page.goto('https://example.com');
   await expect(page).toHaveTitle('Example Domain');
-  await page.waitForURL('https://example.com');
 });
 
 // test('basic failed test', async ({ page }) => {
