35e92a8c559e6b4e6fe4c65e85a702853fa98b3c
Denis Smirnov <dsmirnov@tula.co>
2025-11-04 18:06:21 +0100

    add a failed test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -4,3 +4,8 @@ test('basic test', async ({ page }) => {
   await page.goto('https://example.com');
   await expect(page).toHaveTitle('Example Domain');
 });
+
+test('basic failed test', async ({ page }) => {
+  await page.goto('https://example.com');
+  await expect(page).toHaveTitle('Example Domain 1');
+});
