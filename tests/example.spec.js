30229bd779f6bae5102d59dff5d3d05bb6042687
Denis Smirnov <dsmirnov@tula.co>
2025-11-04 20:09:57 +0100

    add waitForSelector to basic test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('basic test', async ({ page }) => {
   await page.goto('https://example.com');
+  await page.waitForSelector('body');
   await expect(page).toHaveTitle('Example Domain');
 });
 
