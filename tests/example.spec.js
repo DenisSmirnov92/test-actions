e69a46c145738097ae752b0b9d9244f2c3c801b4
Denis Smirnov <denissmirnov92@gmail.com>
2025-11-05 14:06:50 +0100

    add fake test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -9,3 +9,8 @@ test('basic test 2', async ({ page }) => {
   await page.goto('https://example.com');
   await expect(page).toHaveTitle('Example Domain');
 });
+
+test('basic test 3', async ({ page }) => {
+  await page.goto('https://example.com');
+  await expect(page).toHaveTitle('Example Domain');
+});
